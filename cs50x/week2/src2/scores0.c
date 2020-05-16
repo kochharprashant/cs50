d0ad01791917b132ddec8652d8dea49efd2984f2
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 22:52:56 +0530

    Create scores0.c

---
+++ cs50x/week2/src2/scores0.c
@@ -0,0 +1,16 @@
+// Average of three numbers
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Scores
+    
+    int score1 = 72;
+    int score2 = 73;
+    int score3 = 33;
+    
+    // Print average
+    printf("Average: %i\n", (score1 + score2 + score3) / 3);
+}
