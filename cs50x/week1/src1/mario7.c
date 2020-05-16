d707b61c04fdf12bf5c3cde08867d677880e8b7b
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 17:54:42 +0530

    Create mario7.c

---
+++ cs50x/week1/src1/mario7.c
@@ -0,0 +1,12 @@
+// Prints a 3-by-3 grid of bricks with a loop
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        printf("###\n");
+    }
+}
