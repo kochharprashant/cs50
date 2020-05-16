81a25fff118bb9714d8db3e06d4ad8f7490eace9
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 17:52:54 +0530

    Create mario1.c

---
+++ cs50x/week1/src1/mario1.c
@@ -0,0 +1,13 @@
+// Print a row of 4 question marks with a loop
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+}
