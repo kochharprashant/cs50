21aef0f112f7fdd880d44685e6a3f7d8d0aedb63
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 17:55:15 +0530

    Create overflow.c

---
+++ cs50x/week1/src1/overflow.c
@@ -0,0 +1,15 @@
+// Integer overflow
+
+#include <stdio.h>
+#include <cs50.h>
+#include <unistd.h>
+
+int main(void)
+{
+    // Iteratively double i
+    for (int i = 1; ; i *= 2)
+    {
+        printf("%i\n", i);
+        sleep(1);
+    }
+}
