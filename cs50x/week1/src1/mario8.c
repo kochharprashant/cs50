999e1082c3e831cc607d9110524dc39a1a5d0712
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 17:54:57 +0530

    Create mario8.c

---
+++ cs50x/week1/src1/mario8.c
@@ -0,0 +1,18 @@
+// Prints n-by-n grid of bricks with a loop
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    for (int i = 0; i < n; i++)
+    {
+    
+    }
+}
