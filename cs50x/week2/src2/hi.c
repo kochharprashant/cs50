163d465696a52667b5ad47afa00bc44bde816af9
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 22:52:13 +0530

    Create hi.c

---
+++ cs50x/week2/src2/hi.c
@@ -0,0 +1,12 @@
+// Prints ASCII codes
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+    printf("%i %i %i\n", c1, c2, c3);
+}
