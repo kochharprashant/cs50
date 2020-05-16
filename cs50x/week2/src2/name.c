17307c150fda6005ce7e35626f438af666a2fba6
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 22:52:35 +0530

    Create name.c

---
+++ cs50x/week2/src2/name.c
@@ -0,0 +1,17 @@
+// Prints ASCII codes
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    char c1 = 'P';
+    char c2 = 'R';
+    char c3 = 'A';
+    char c4 = 'S';
+    char c5 = 'H';
+    char c6 = 'A';
+    char c7 = 'N';
+    char c8 = 'T';
+    printf("%i %i %i %i %i %i %i %i\n", c1, c2, c3, c4, c5, c6, c7, c8);
+}
