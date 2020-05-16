54e9dfa434a98d9e9cbe7ac05deffc9cea3c3fd8
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-16 12:24:57 +0530

    Create conditions.c

---
+++ cs50x/week1/src1/conditions.c
@@ -0,0 +1,27 @@
+// Conditions and relational operators
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Prompt user for x
+    int x = get_int("x: ");
+    
+    // Prompt user for y
+    int y = get_int("y: ");
+    
+    // Compare x and y
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
+}
