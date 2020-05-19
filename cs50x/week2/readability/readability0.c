ab1c64ebed9832c80cefa19c1545b35523de7f64
Prashant Kochhar <64054298+kochharprashant@users.noreply.github.com>
2020-05-19 21:48:26 +0530

    Create readability0.c

---
+++ cs50x/week2/readability/readability0.c
@@ -0,0 +1,93 @@
+// Readability problem set
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h> 
+
+int main(void)
+{
+    // Prompt user for text
+    string text = get_string("Text: ");
+    
+    // Define integar for letters, words, sentences and grade
+    int letters = 0, words = 1, sentences = 0;
+    
+    // Check text for letters, words and sentences
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if(isalpha(text[i]))
+        {
+            // Counter for letters
+            letters++;
+        }
+        else if(isblank(text[i]))
+        {
+            // Counter for words
+            words++;
+        }
+        else if(text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            // Counter for sentences
+            sentences++;
+        }
+    }
+        
+        // Average of letters
+        float L = 100.0 * letters / words;
+        
+        // Average of sentences
+        float S = 100.0 * sentences / words;
+        
+        // Coleman-Liau index formula for grading
+        float grade = 0.0588 * L - 0.296 * S - 15.8;
+        
+        // Check grade is > 16
+        if (grade > 16)
+        {
+            // Print the number of letters
+            printf("%i letter(s)\n", letters);
+        
+            // Print the number of words
+            printf("%i word(s)\n", words);
+        
+            // Print the number of sentences
+            printf("%i sentence(s)\n", sentences);
+            
+            // Print grade
+            printf("Grade 16+\n");
+        }
+        
+        // Check grade is between 1 to 16
+        else if (grade <= 16 && grade >= 0)
+        {
+            // Print the number of letters
+            printf("%i letter(s)\n", letters);
+        
+            // Print the number of words
+            printf("%i word(s)\n", words);
+        
+            // Print the number of sentences
+            printf("%i sentence(s)\n", sentences);
+            
+            // Print grade
+            printf("Grade %.f\n", round(grade));
+        }
+        
+        // Check grade is below 1
+        else
+        {
+            // Print the number of letters
+            printf("%i letter(s)\n", letters);
+        
+            // Print the number of words
+            printf("%i word(s)\n", words);
+        
+            // Print the number of sentences
+            printf("%i sentence(s)\n", sentences);
+            
+            // Print grade
+            printf("Before Grade 1\n");
+        }
+}
